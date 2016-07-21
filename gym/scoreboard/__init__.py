8f4ce6fba5cc5b17279ebe8c3a26fbfc19466ea6
Alex Broad <alexsbroad@gmail.com>
2016-07-21 15:26:49 -0500

    updates to incorporate new car racing environment

--- gym/scoreboard/__init__.py
+++ gym/scoreboard/__init__.py
@@ -371,6 +371,25 @@ left to right: true speed, four ABS sensors, steering wheel position, gyroscope.
 """
 )
 
+add_task(
+    id='CarRacing-v1',
+    group='box2d',
+    experimental=True,
+    contributor='olegklimov',
+    summary='Race a car around a track.',
+    description="""
+Easiest continuous control task to learn from pixels, a top-down racing environment.
+Discreet control is reasonable in this environment as well, on/off discretisation is
+fine. State consists of 96x96 pixels. Reward is -0.1 every frame and +1000/N for every track
+tile visited, where N is the total number of tiles in track. For example, if you have
+finished in 732 frames, your reward is 1000 - 0.1*732 = 926.8 points.
+Episode finishes when all tiles are visited.
+Some indicators shown at the bottom of the window and the state RGB buffer. From
+left to right: true speed, four ABS sensors, steering wheel position, gyroscope.
+Augmented
+"""
+)
+
 # mujoco
 
 add_task(
